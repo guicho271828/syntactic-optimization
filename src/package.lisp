84fa251dcab66b80b2620f5f838a254b3d24d9da
Masataro Asai <guicho2.71828@gmail.com>
2017-11-07 10:17:01 +0900

    nested map

--- src/package.lisp
+++ src/package.lisp
@@ -115,20 +115,87 @@
 ;;   98.41% CPU
 ;;   378,495,072 processor cycles
 ;;   3,211,264 bytes consed
+
+(defvar *optimize-nested-map* nil)
+
+(sb-ext:without-package-locks
+  (define-compiler-macro map (&whole whole result-type function list &rest more-lists)
+    ;; (map fn1 args1* (map fn2 args2*) args3*)
+    ;; == (map (lambda (args1* args2* args3*)
+    ;;              (fn1 args1* (fn2 args2*) args3*))
+    ;;            args1* args2* args3*)
+    (if (and result-type (constantp result-type) *optimize-nested-map*)
+        (optimize-nested-map whole result-type function (cons list more-lists))
+        whole)))
+
+(defun optimize-nested-map (whole result-type function args)
+  (iter (for arg in args)
+        (with matched = nil)
+        (match arg
+          (`(map nil ,@_)
+            (return-from optimize-nested-map whole))
+          (`(map ,(guard result-type (constantp result-type)) ,fn2 ,@args2)
+            (setf matched t)
+            (let ((args-gs (make-gensym-list (length args2) "ARG")))
+              (appending args-gs into lambda-args)
+              (collecting `(funcall ,fn2 ,@args-gs) into inner)
+              (appending args2 into outer)))
+          (_
+           (let ((args-gs (make-gensym "ARG")))
+             (collecting args-gs into lambda-args)
+             (collecting args-gs into inner)
+             (collecting args into outer))))
+        (finally
+         (return
+           (if matched
+               `(map ,result-type (lambda ,lambda-args (funcall ,function ,@inner)) ,@outer)
+               whole)))))
+
+(setf *optimize-nested-map* nil)
+(defun fn3 ()
+  (let ((list (make-list *len* :initial-element 0)))
+    (map 'vector #'fn
+         (map 'vector #'fn 
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list))
+         (map 'vector #'fn 
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list))
+         (map 'vector #'fn 
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)))))
+
+(setf *optimize-nested-map* t)
+(defun fn4 ()
+  (let ((list (make-list *len* :initial-element 0)))
+    (map 'vector #'fn
+         (map 'vector #'fn 
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list))
+         (map 'vector #'fn 
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list))
+         (map 'vector #'fn 
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)
+              (map 'vector #'fn list list list)))))
+
+(time (fn3))
 ;; Evaluation took:
-;;   0.074 seconds of real time
-;;   0.072000 seconds of total run time (0.072000 user, 0.000000 system)
-;;   97.30% CPU
-;;   222,508,480 processor cycles
-;;   20,775,360 bytes consed
-;;   
-;; NIL
-;; SYNTACTIC-OPTIMIZATION> (time (progn (fn2) nil))
+;;   0.135 seconds of real time
+;;   0.132000 seconds of total run time (0.132000 user, 0.000000 system)
+;;   97.78% CPU
+;;   404,698,822 processor cycles
+;;   11,999,696 bytes consed
+(time (fn4))
 ;; Evaluation took:
-;;   0.073 seconds of real time
-;;   0.072000 seconds of total run time (0.072000 user, 0.000000 system)
-;;   98.63% CPU
-;;   216,970,028 processor cycles
-;;   15,990,784 bytes consed
-;;   
-;; NIL
+;;   0.133 seconds of real time
+;;   0.136000 seconds of total run time (0.136000 user, 0.000000 system)
+;;   102.26% CPU
+;;   399,812,344 processor cycles
+;;   2,405,648 bytes consed
